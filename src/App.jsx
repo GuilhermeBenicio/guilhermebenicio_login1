f60d02bb08ba7fbd30ab2391ea2e124504392518
Guilherme Benicio <guilhermebenicio18102003@gmail.com>
2023-08-30 00:30:38 -0300

    tela login - versão mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import logotipo from './assets/logotipo.jpg';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="logotipo">
+        <img src={logotipo} alt="" />
+      </div>
+      <div className="conteudo">
+        <p className="description">
+          Selecione seu perfil abaixo para acessar sua conta:
+        </p>
+        <div className="botoes">
+          <button className="btnResponsavel">Responsável</button>
+          <button className="btnProfissional">Profissional</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
